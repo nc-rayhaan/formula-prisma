31d4153c2e76f4a43f3aacb7f036aeab7176f721
Rayhaan Ugharadar <rayhaan.ugharadar@northcoders.com>
2025-04-28 15:29:53 +0100

    Adds parameters to functions

--- sprint.ts
+++ sprint.ts
@@ -7,7 +7,7 @@ export async function getEngineProviders() {}
 
 // 2: Write a function that takes an engineProviderId as an argument and returns all the teams that use that specific engine.
 
-export async function getTeamsByEngineProvider() {}
+export async function getTeamsByEngineProvider(engineProviderId: number) {}
 
 // 3: Write a function that returns all drivers whose nationality is British
 
@@ -15,4 +15,4 @@ export async function getBritishDrivers() {}
 
 /* 4: Write a function that will take a race id as an argument and returns the name of the grand prix, information about the circuit and the podium (position 1, 2 and 3) from that race. Please have a look at the test to see the structure of the expected returned object
  */
-export async function getRaceResultByRaceId() {}
+export async function getRaceResultByRaceId(raceId: number) {}
