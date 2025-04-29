dfae38b1f87574b403e1ddebb9f51873041b6904
Rayhaan Ugharadar <rayhaan.ugharadar@northcoders.com>
2025-04-29 08:45:18 +0100

    Refactors tests to tidy them up

--- __tests__/sprint.test.ts
+++ __tests__/sprint.test.ts
@@ -28,49 +28,19 @@ describe("getEngineProviders(", () => {
 describe.skip("getBritishDrivers", () => {
   test("returns a list of british drivers", async () => {
     const britishDrivers: Driver[] = await getBritishDrivers();
-
-    expect(britishDrivers).toEqual([
-      {
-        id: 1,
-        firstName: "Lewis",
-        lastName: "Hamilton",
+    expect(britishDrivers).toHaveLength(4);
+    britishDrivers.forEach((driver) => {
+      expect(driver).toEqual({
+        id: expect.any(Number),
+        firstName: expect.any(String),
+        lastName: expect.any(String),
         nationality: "British",
         dateOfBirth: expect.any(Date),
-        points: 4865,
-        championships: 7,
-        teamId: 1,
-      },
-      {
-        id: 4,
-        firstName: "Lando",
-        lastName: "Norris",
-        nationality: "British",
-        dateOfBirth: expect.any(Date),
-        points: 596,
-        championships: 0,
-        teamId: 5,
-      },
-      {
-        id: 5,
-        firstName: "George",
-        lastName: "Russell",
-        nationality: "British",
-        dateOfBirth: expect.any(Date),
-        points: 421,
-        championships: 0,
-        teamId: 1,
-      },
-      {
-        id: 22,
-        firstName: "Oliver",
-        lastName: "Bearman",
-        nationality: "British",
-        dateOfBirth: expect.any(Date),
-        points: 7,
-        championships: 0,
-        teamId: 8,
-      },
-    ]);
+        points: expect.any(Number),
+        championships: expect.any(Number),
+        teamId: expect.any(Number),
+      });
+    });
   });
 });
 
@@ -78,24 +48,17 @@ describe.skip("getTeamsByEngineProvider", () => {
   test("returns an array of teams and engine provider info when passed an engineProviderId", async () => {
     const teamsUsingHondaEngines: TeamWithEngineProvider[] =
       await getTeamsByEngineProvider(4);
-    expect(teamsUsingHondaEngines).toEqual([
-      {
-        id: 4,
-        name: "Oracle Red Bull Racing",
-        base: "Milton Keynes, United Kingdom",
-        principal: "Christian Horner",
-        engineProviderId: 4,
+    expect(teamsUsingHondaEngines).toHaveLength(2);
+    teamsUsingHondaEngines.forEach((team) => {
+      expect(team).toEqual({
+        id: expect.any(Number),
+        name: expect.any(String),
+        base: expect.any(String),
+        principal: expect.any(String),
+        engineProviderId: expect.any(Number),
         engineProvider: { id: 4, name: "Honda" },
-      },
-      {
-        id: 10,
-        name: "Visa Cash App RB Formula One Team",
-        base: "Faenza, Italy",
-        principal: "Laurent Mekies",
-        engineProviderId: 4,
-        engineProvider: { id: 4, name: "Honda" },
-      },
-    ]);
+      });
+    });
   });
 });
 
@@ -103,56 +66,21 @@ describe.skip("getRacesWonByDriver", () => {
   test("returns a list of races won by a chosen driver including information about the races", async () => {
     const racesWonByLandoNorris: RaceWithWinningDriver[] =
       await getRacesWonByDriver(4);
-    expect(racesWonByLandoNorris).toEqual([
-      {
+    expect(racesWonByLandoNorris).toHaveLength(4);
+    racesWonByLandoNorris.forEach((race) => {
+      expect(race).toEqual({
         race: {
-          id: 19,
-          grandPrixName: "Miami Grand Prix",
-          circuitId: 19,
+          id: expect.any(Number),
+          grandPrixName: expect.any(String),
+          circuitId: expect.any(Number),
           date: expect.any(Date),
         },
         driver: {
           firstName: "Lando",
           lastName: "Norris",
         },
-      },
-      {
-        race: {
-          id: 15,
-          grandPrixName: "Dutch Grand Prix",
-          circuitId: 15,
-          date: expect.any(Date),
-        },
-        driver: {
-          firstName: "Lando",
-          lastName: "Norris",
-        },
-      },
-      {
-        race: {
-          id: 6,
-          grandPrixName: "Singapore Grand Prix",
-          circuitId: 6,
-          date: expect.any(Date),
-        },
-        driver: {
-          firstName: "Lando",
-          lastName: "Norris",
-        },
-      },
-      {
-        race: {
-          id: 4,
-          grandPrixName: "Abu Dhabi Grand Prix",
-          circuitId: 4,
-          date: expect.any(Date),
-        },
-        driver: {
-          firstName: "Lando",
-          lastName: "Norris",
-        },
-      },
-    ]);
+      });
+    });
   });
 });
 
